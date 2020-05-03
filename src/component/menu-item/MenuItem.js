6bce7d927e8148f402af46f0aa2f1e70eb87174a
William ANNE <anne.william95@gmail.com>
2020-05-03 13:35:32 +0200

    Restructuring app + a little bit of styling

--- src/component/menu-item/MenuItem.js
+++ src/component/menu-item/MenuItem.js
@@ -3,8 +3,9 @@ import './MenuItem.scss'
 
 const MenuItem = ({title, subtitle, imageUrl, size}) => {
     return (
-            <div className={`size : ${size} menu-item`} 
-                style={{backgroundImage : `url(${imageUrl})`}}>
+            <div className={`size : ${size} menu-item`} >
+                    <div className='background-image' 
+                         style={{backgroundImage : `url(${imageUrl})`}}/>
                 <div className='content'>
                     <h1 className='title'>{title}</h1>
                     <span className='subtitle'>{subtitle}</span>

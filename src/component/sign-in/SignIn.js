20d82373acd82eba8cff6ec0591fcfc4a2c97d65
William ANNE <anne.william95@gmail.com>
2020-05-05 17:39:30 +0200

    Finishing sign in with email

--- src/component/sign-in/SignIn.js
+++ src/component/sign-in/SignIn.js
@@ -3,7 +3,7 @@ import './SignIn.scss'
 import '../form-input/form-input'
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/CustomButton';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn extends Component 
 {
@@ -17,10 +17,19 @@ class SignIn extends Component
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        const {email, password} = this.state;
 
-        this.setState({email:'', password:''});
+        try
+        {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'', password:''});
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
     }
     
     handleChange = event => {

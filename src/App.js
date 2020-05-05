db0b81e1d449b42db78a9ddd68109ea924f491c7
William ANNE <anne.william95@gmail.com>
2020-05-05 15:03:35 +0200

    Database storage firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from './page/homepage/Homepage'
 import ShopPage from './page/shop/Shop';
 import Header from './component/header/Header'
 import SignInAndSignUp from './page/sign-in-and-sign-up/SignInAndSignUp'
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
 
@@ -20,8 +20,25 @@ class App extends React.Component {
 
   componentDidMount()
   {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>
-      this.setState({ currentUser : user}));
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth)
+      {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser : {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+        });
+      }
+      else
+      {
+        this.setState({currentUser : userAuth});
+      }
+    });
   }
 
   componentWillUnmount()
--- src/page/sign-in-and-sign-up/SignInAndSignUp.js
+++ src/page/sign-in-and-sign-up/SignInAndSignUp.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './SignInAndSignUp.scss'
 import SignIn from '../../component/sign-in/SignIn'
+import SignUp from '../../component/sign-up/SignUp'
 
 const SignInAndSignUp = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 );
 

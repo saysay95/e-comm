8c75452b383967ceecf579e215cc65da01970d25
William ANNE <anne.william95@gmail.com>
2020-05-05 23:14:55 +0200

    Redirect of signed in users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import Homepage from './page/homepage/Homepage'
 import ShopPage from './page/shop/Shop';
@@ -49,15 +49,23 @@ class App extends React.Component {
         <Switch>
             <Route exact path='/' component={Homepage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/signin' component={SignInAndSignUp} />
+            <Route path='/signin' 
+                   render={() => this.props.currentUser ? (
+                     <Redirect to='/' />
+                     ) : (
+                       <SignInAndSignUp/> ) }/>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
